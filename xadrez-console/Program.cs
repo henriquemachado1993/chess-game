fe0b3bdb051ef8be92d4ea8ca6015214c5ba7edc
Henrique Machado <henrique.santos@basis.com.br>
2018-10-24 13:47:12 -0300

    Criação da camada tabuleiro, e camada xadrez;

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -7,7 +8,15 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3,4);
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Amarelo), new Posicao(0,0));
+            tab.colocarPeca(new Torre(tab, Cor.Amarelo), new Posicao(1,3));
+            tab.colocarPeca(new Rei(tab, Cor.Amarelo), new Posicao(2,4));
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas ) {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
+    }
+}
