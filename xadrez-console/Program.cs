6e8ab8d11c5dd62923317349ae7246619fd32116
Henrique Machado <henriquemachado1993@gmail.com>
2018-10-25 20:35:22 -0300

    Continuacao do jogo de xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,22 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Amarelo), new Posicao(0,0));
-            tab.colocarPeca(new Torre(tab, Cor.Amarelo), new Posicao(1,3));
-            tab.colocarPeca(new Rei(tab, Cor.Amarelo), new Posicao(2,4));
+                tab.colocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Vermelha), new Posicao(0, 2));
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
         }
